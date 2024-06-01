573c8dad93dff40f9db961a3dbbd95baa6b8a648
kazi fahim <kazifahim661@gmail.com>
2024-06-01 22:16:07 +0600

    theme controler aded

--- src/Pages/HOme.jsx
+++ src/Pages/HOme.jsx
@@ -1,10 +1,12 @@
 import Banner from "../componests/Banner";
+import Promote from "../componests/Promote";
 
 
 const HOme = () => {
     return (
         <div>
             <Banner></Banner>
+            <Promote></Promote>
         </div>
     );
 };
